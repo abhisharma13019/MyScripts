9eef4c517d03c57c32a7e556f019de0f1717604d
Abhishek Sharma <abhisharma13019@gmail.com>
2025-09-10 16:52:09 +0530

    simple number guessing game using until loop

---
+++ 12_UntilLoop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Number guessing game using until loop
+target=$(( RANDOM % 10 +1 ))
+a=0
+echo " Wlc to number guessing game"
+until [ "$a" -eq "$target" ]
+do
+read -p "enter the number: " a
+if [ $a -eq $target ]
+then
+echo "you win"
+elif [ $a -gt $target ]
+then
+echo "number is to high"
+else
+echo "number is low"
+fi
+done
